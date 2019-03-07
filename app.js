367f82c3e638b7bcabbdc204a8cdf9b567e9befe
slcassidy <samantha.l.cassidy@gmail.com>
2019-03-07 08:11:03 -0500

    Upload 20190703

--- app.js
+++ app.js
@@ -4,14 +4,20 @@ const stocksList = ['FB', 'AAPL', 'TSLA', 'GOOGL'];
 const validationList = [];
 
 // Hide the Modal at the beginning
+const hideMod = function(){
 $(`.modal-dialog`).hide();
+console.log("Hide at the top");
+};
+
+hideMod();
+
 
 // Global Variables
 let newsArticle = "";
 let logoSymbol = "";
 // Populate a list of all the symbols
 
-const validate = function(){
+const validate = function () {
   // event.preventDefault(); 
 
 
@@ -21,22 +27,22 @@ const validate = function(){
     url: symbolURL,
     method: 'GET'
   })
-  .then(function(response) {
+    .then(function (response) {
 
-    // console.log(response);
-    // console.log(response[101].symbol);
-    
-    for(let i = 0; i < response.length; i++){
-    
-      const stockSymbol = (response[i].symbol);
-      // console.log(stockSymbol); 
-      // console.log([i]); 
-      validationList.push(stockSymbol);
+      // console.log(response);
+      // console.log(response[101].symbol);
 
-    }
-    // console.log(validationList);
-    
-  })
+      for (let i = 0; i < response.length; i++) {
+
+        const stockSymbol = (response[i].symbol);
+        // console.log(stockSymbol); 
+        // console.log([i]); 
+        validationList.push(stockSymbol);
+
+      }
+      // console.log(validationList);
+
+    })
 
 
 };
@@ -54,14 +60,14 @@ $(document).ready(validate);
 //         url: logoURL,
 //         method: 'GET'
 //       })
-      
+
 //       .then(function(response) {
-  
+
 //         console.log(response.url)
 //         const stockSid = $('<div>').addClass('stockSide');
 //         const logoSymbol = $(`<img src="${response.url}">`)
-        
-  
+
+
 //         stockSid.append(logoSymbol);
 //         $(`#stock-side`).prepend(logoSymbol)
 //       }) //End of logoURL
@@ -70,107 +76,107 @@ $(document).ready(validate);
 
 
 // Here we run our AJAX call to the EXTrading API
-const displayInfo = function(event){
+const displayInfo = function (event) {
 
-event.preventDefault(); 
+  event.preventDefault();
 
-// Get the symbol used to research
+  // Get the symbol used to research
 
-const symbol = $(this).attr('data-name');
-const queryURL = `https://api.iextrading.com/1.0/stock/${symbol}/batch?types=quote,news&range=1m&last=1`;
+  const symbol = $(this).attr('data-name');
+  const queryURL = `https://api.iextrading.com/1.0/stock/${symbol}/batch?types=quote,news&range=1m&last=1`;
 
-const logoURL = `https://api.iextrading.com/1.0/stock/${symbol}/logo`;
+  const logoURL = `https://api.iextrading.com/1.0/stock/${symbol}/logo`;
 
-const newsURL = `https://api.iextrading.com/1.0/stock/${symbol}/news`;
+  const newsURL = `https://api.iextrading.com/1.0/stock/${symbol}/news`;
 
-$.ajax({
-  url: queryURL,
-  method: 'GET'
-})
+  $.ajax({
+    url: queryURL,
+    method: 'GET'
+  })
 
-.then(function(response) {
-
-    console.log(response);  
-    // Creating a div to hold the stock
-    const stockDiv = $('<div>').addClass('stock');
-
-    // Storing the company name
-    const companyName = response.quote.companyName;
-
-    // Creating an element to display the company name
-    const nameHolder = $('<h3>').text(`Company Name: ${companyName}`);
-    
-    // Appending the name to our stockDiv
-    stockDiv.append(nameHolder);
-    
-    // Storing the price
-    const stockPrice = response.quote.latestPrice;
-
-    // Creating an element to display the price
-    const priceHolder = $('<p>').text(`Stock Price: $${stockPrice}`);
-
-    // Appending the price to our stockDiv
-    stockDiv.append(priceHolder);
-
-
-    
-
-    // Finally adding the stockDiv to the DOM
-    // Until this point nothing is actually displayed on our page
-    $('#stock-info').prepend(`<hr>`);
-    $('#stock-info').prepend(stockDiv);
-    // getLogo ();
-  
-   
-
-    count = 0;
-    // News 10
-    $.ajax({
-      url: newsURL,
-      method: 'GET'
-    })
-    
-    .then(function(response) {
+    .then(function (response) {
 
       console.log(response);
+      // Creating a div to hold the stock
+      const stockDiv = $('<div>').addClass('stock');
 
-      for(let i = 0; i < 10; i++){
-    
-        // global newsArticle above 
-        newsArticle = (response[i].headline);
-        console.log(newsArticle); 
-        console.log([i]); 
+      // Storing the company name
+      const companyName = response.quote.companyName;
 
-        // Creating an element to display the news summary
-        const summaryHolder = $('<p>').text(`News Headline: ${newsArticle}`);
+      // Creating an element to display the company name
+      const nameHolder = $('<h3>').text(`Company Name: ${companyName}`);
 
-        // Appending the summary to our stockDiv
-        stockDiv.append(summaryHolder);
-        count++
-        }
+      // Appending the name to our stockDiv
+      stockDiv.append(nameHolder);
 
-    }) //End News
+      // Storing the price
+      const stockPrice = response.quote.latestPrice;
 
-     // Logo
-     $.ajax({
-      url: logoURL,
-      method: 'GET'
-    })
-    
-    .then(function(response) {
+      // Creating an element to display the price
+      const priceHolder = $('<p>').text(`Stock Price: $${stockPrice}`);
 
-      console.log(response.url)
-      // const stockSid = $('<div>').addClass('stock');
-      console.log(count);
-      const logoSymbol = $(`<img class="image"src="${response.url}">`)
-      
+      // Appending the price to our stockDiv
+      stockDiv.append(priceHolder);
 
-      // stockDiv.append(logoSymbol);
-      // $(`#stock-side`).prepend(logoSymbol);
-      $(`#stock-info`).prepend(logoSymbol);
-    }) //End of logoURL
 
-})  //end of queryURL
+
+
+      // Finally adding the stockDiv to the DOM
+      // Until this point nothing is actually displayed on our page
+      $('#stock-info').prepend(`<hr>`);
+      $('#stock-info').prepend(stockDiv);
+      // getLogo ();
+
+
+
+      count = 0;
+      // News 10
+      $.ajax({
+        url: newsURL,
+        method: 'GET'
+      })
+
+        .then(function (response) {
+
+          console.log(response);
+
+          for (let i = 0; i < 10; i++) {
+
+            // global newsArticle above 
+            newsArticle = (response[i].headline);
+            console.log(newsArticle);
+            console.log([i]);
+
+            // Creating an element to display the news summary
+            const summaryHolder = $('<p>').text(`News Headline: ${newsArticle}`);
+
+            // Appending the summary to our stockDiv
+            stockDiv.append(summaryHolder);
+            count++
+          }
+
+        }) //End News
+
+      // Logo
+      $.ajax({
+        url: logoURL,
+        method: 'GET'
+      })
+
+        .then(function (response) {
+
+          console.log(response.url)
+          // const stockSid = $('<div>').addClass('stock');
+          console.log(count);
+          const logoSymbol = $(`<img class="image"src="${response.url}">`)
+
+
+          // stockDiv.append(logoSymbol);
+          // $(`#stock-side`).prepend(logoSymbol);
+          $(`#stock-info`).prepend(logoSymbol);
+        }) //End of logoURL
+
+    })  //end of queryURL
 
 };
 
@@ -187,72 +193,73 @@ const renderButtons = function () {
 
   // Looping through the array of stocks
   for (let i = 0; i < stocksList.length; i++) {
-// Creating the button
-   const newButton = $('<button type="submit" class="btn btn-default">');
-  //  const space = $(`&nbsp`);
-  // Add type="submit" class="btn btn-default" to the button to make it look nicer
-    
+    // Creating the button
+    const newButton = $('<button type="submit" class="btn btn-default">');
+    //  const space = $(`&nbsp`);
+    // Add type="submit" class="btn btn-default" to the button to make it look nicer
+
     // Adding a class of stock-btn to our button
     newButton.addClass('stock-btn');
-    
+
     // Adding a data-attribute
     newButton.attr('data-name', stocksList[i]);
-    
+
     // Providing the initial button text
     newButton.text(stocksList[i]);
-    
-      // const space = $(`&nbsp`).html();
-      const space = (`<i>  </i>`);
-  // $('#buttons-view').append(space);
-    
+
+    // const space = $(`&nbsp`).html();
+    const space = (`<i>  </i>`);
+    // $('#buttons-view').append(space);
+
     // Adding the button to the buttons-view div
     $('#buttons-view').append(newButton, space);
-    
+
   }
 };
 
 // Clicking on the button creates a new button
-const addButton = function(event) {
- 
+const addButton = function (event) {
+
   // event.preventDefault() prevents the form from trying to submit itself.
   event.preventDefault();
- 
+
 
   // This line will grab the text from the input box and make upper case as well as trim no spaces
   const stock = $('#input').val().trim().toUpperCase();
 
-    // The stock from the text box is then added to our array
-  
-   
-    var check = 0;
-    console.log(`beginning ${check}`);
-  for(let i = 0; i < validationList.length; i++){
+  // The stock from the text box is then added to our array
+
+
+  var check = 0;
+  console.log(`beginning ${check}`);
+  for (let i = 0; i < validationList.length; i++) {
     const checkSymbol = validationList[i];
     // console.log(checkSymbol); 
     console.log(`in for loop ${check}`);
-    if(checkSymbol === stock){
+    if (checkSymbol === stock) {
       check = 1;
       break;
-    // }else{
-    //   check = true;
-    //   console.log(`Not equal ${check}`);
+      // }else{
+      //   check = true;
+      //   console.log(`Not equal ${check}`);
     }
 
-  };  
+  };
 
   // Make sure the new Stock is not pushed if  
   // console.log(stocksList);
   console.log(`before append stock button ${check}`);
-  if(check === 0){
+  if (check === 0) {
     // alert("Stock symbol is not valid. Please try again");
-
+    // Calling the Modal
     $('#myModal').on('shown.bs.modal', function () {
-      $('#myInput').trigger('focus')     
+      $('#myInput').trigger('focus')
     });
+    // Allowing the display of th Modal
     $('.modal-dialog').show();
     console.log("show up!!");
 
-  }else{
+  } else {
     stocksList.push(stock);
   };
 
@@ -273,22 +280,22 @@ $('#buttons-view').on('click', '.stock-btn', displayInfo);
 // Calling the renderButtons function to display the initial buttons
 renderButtons();
 
-  // Function to empty out the articles
-  const clear = function () {
-    $('#stock-info').empty();
-  }
+// Function to empty out the articles
+const clear = function () {
+  $('#stock-info').empty();
+}
 
 
-  //  .on('click') function associated with the clear button
-  $('#clear-all').on('click', clear);
+//  .on('click') function associated with the clear button
+$('#clear-all').on('click', clear);
 
-  // Clear the Alert Button
-  $(`#alertB`).on("click", function(){
-    $(`.modal-dialog`).hide();
+$(`.close`).on("click", function () {
+  hideMod();
+});
 
-  })
+// Clear the Alert Button
+$(`#alertB`).on("click", function () {
+  hideMod();
 
-
-
-
+});
 
